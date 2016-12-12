b0014ec8f7fee883cb3f8c736ea767a818c00cb5
Anatolii Kushnir <akush@softserveinc.com>
2016-12-12 17:11:13 +0200

    try to commit #2

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -23,11 +23,11 @@ module.exports = {
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-        compress : {
-            warnings : false,
-        }
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //     compress : {
+    //         warnings : false,
+    //     }
+    // }),
     new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('production')
     }),
