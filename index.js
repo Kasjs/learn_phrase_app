1a38295d6b652454900798c1f633dbcbf5050635
Anatolii Kushnir <akush@softserveinc.com>
2016-12-13 12:48:50 +0200

    new deploy

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ if (isDeveloping) {
     app.use(middleware);
     app.use(webpackHotMiddleware(compiler));
     app.use(express.static('./public'));
-    app.use(express.static('./dist'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({
         extended: true
