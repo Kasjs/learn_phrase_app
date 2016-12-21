392c751a9ecda71919526ae21d1796af6da47fd6
Anatolii Kushnir <akush@softserveinc.com>
2016-12-21 13:00:22 +0200

    new deploy

--- index.js
+++ index.js
@@ -16,12 +16,11 @@ React = require('react'),
 Router = require('react-router'),
 config = require('./config'),
 
-isDeveloping = process.env.NODE_ENV !== 'production',
-port = isDeveloping ? 3000 : process.env.PORT,
+isProduction = process.env.NODE_ENV === 'production',
+port = isProduction ? process.env.PORT : process.env.PORT,
 app = express();
 app.use(passport.initialize());
 
-app.use(favicon(path.join(__dirname, 'server', 'assets', 'images', 'favicon.ico')));
 
 require('node-jsx').install();
 require('./server/models/User');
@@ -30,7 +29,7 @@ require('./server/passport')(config);
 const routes = require('./server/routes/index');
 const authCheckMiddleware = require('./server/middlewares/auth-check')(config);
 
-if (isDeveloping) {
+if (isProduction) {
     const compiler = webpack(webpackConfig);
     const middleware = webpackMiddleware(compiler, {
         publicPath: webpackConfig.output.publicPath,
@@ -50,9 +49,12 @@ if (isDeveloping) {
         console.log('Error: Could not connect to MongoDB');
     });
 
+    app.use(favicon(path.join(__dirname, 'server', 'assets', 'images', 'favicon.ico')));
     app.use(middleware);
     app.use(webpackHotMiddleware(compiler));
     app.use(express.static('./public'));
+    app.use(express.static('./server'));
+    app.use(express.static('./dist'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({
         extended: true
@@ -74,9 +76,9 @@ if (isDeveloping) {
 
 } else {
 
-    app.use(express.static(__dirname + './dist'));
+    app.use(express.static(__dirname + '/dist'));
     app.get('*', function response(req, res) {
-      res.sendFile(path.join(__dirname, './dist/index.html'));
+      res.sendFile(path.join(__dirname, 'dist/index.html'));
     });
 
     app.use('/', routes);
